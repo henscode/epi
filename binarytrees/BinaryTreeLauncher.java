9d2247110790251ac85fbd1abd6b7bcc04ea8367
henscode <henscode@gmail.com>
2017-05-17 14:06:20 -0400

    Update BinaryTreeLauncher.java

--- binarytrees/BinaryTreeLauncher.java
+++ binarytrees/BinaryTreeLauncher.java
@@ -2,42 +2,56 @@ package binarytrees;
 
 import binarytrees.BinaryTree.BinaryTreeNode;
 
-public class BinaryTreeLauncher {
-	@SuppressWarnings("unused")
+@SuppressWarnings("unused")
+public class BinaryTreeLauncher {	
 	public static void main(String[] args) {
-		BinaryTreeNode<Integer> one = new BinaryTreeNode<Integer>(1);
-		BinaryTreeNode<Integer> two = new BinaryTreeNode<Integer>(2);
-		BinaryTreeNode<Integer> three = new BinaryTreeNode<Integer>(3);
-		BinaryTreeNode<Integer> four = new BinaryTreeNode<Integer>(4);
-		BinaryTreeNode<Integer> five = new BinaryTreeNode<Integer>(5);
-		BinaryTreeNode<Integer> six = new BinaryTreeNode<Integer>(6);
-		BinaryTreeNode<Integer> seven= new BinaryTreeNode<Integer>(7);
-		BinaryTreeNode<Integer> eight = new BinaryTreeNode<Integer>(8);
-		BinaryTreeNode<Integer> nine = new BinaryTreeNode<Integer>(9);
-		BinaryTreeNode<Integer> ten = new BinaryTreeNode<Integer>(10);
-		BinaryTreeNode<Integer> eleven = new BinaryTreeNode<Integer>(11);
-		BinaryTreeNode<Integer> twelve = new BinaryTreeNode<Integer>(12);
-		BinaryTreeNode<Integer> thirteen = new BinaryTreeNode<Integer>(13);
-		BinaryTreeNode<Integer> fourteen = new BinaryTreeNode<Integer>(14);
+	    BinaryTreeNode<Integer> one = new BinaryTreeNode<Integer>(1);
+	    BinaryTreeNode<Integer> two = new BinaryTreeNode<Integer>(2);
+	    BinaryTreeNode<Integer> three = new BinaryTreeNode<Integer>(3);
+	    BinaryTreeNode<Integer> four = new BinaryTreeNode<Integer>(4);
+	    BinaryTreeNode<Integer> five = new BinaryTreeNode<Integer>(5);
+	    BinaryTreeNode<Integer> six = new BinaryTreeNode<Integer>(6);
+	    BinaryTreeNode<Integer> seven= new BinaryTreeNode<Integer>(7);
+	    BinaryTreeNode<Integer> eight = new BinaryTreeNode<Integer>(8);
+	    BinaryTreeNode<Integer> nine = new BinaryTreeNode<Integer>(9);
+	    BinaryTreeNode<Integer> ten = new BinaryTreeNode<Integer>(10);
+	    BinaryTreeNode<Integer> eleven = new BinaryTreeNode<Integer>(11);
+	    BinaryTreeNode<Integer> twelve = new BinaryTreeNode<Integer>(12);
+	    BinaryTreeNode<Integer> thirteen = new BinaryTreeNode<Integer>(13);
+	    BinaryTreeNode<Integer> fourteen = new BinaryTreeNode<Integer>(14);
 		
-		one.setLeft(two);
-		one.setRight(three);
-		two.setLeft(four);
-		two.setRight(five);
-		three.setLeft(six);
-		three.setRight(seven);
-		four.setLeft(eight);
-		four.setRight(nine);
-		five.setLeft(ten);
-		five.setRight(eleven);
-		six.setLeft(twelve);
-		six.setRight(thirteen);
-		seven.setLeft(fourteen);
-		
-		BinaryTree<Integer> bt = new BinaryTree<Integer>(one);
-		
-		System.out.println("Height: " + bt.height(one));
-		System.out.println("Level Order: ");
-		bt.printLevelOrder(one);
+	    one.setLeft(two);
+	    one.setRight(three);
+	    two.setLeft(four);
+	    two.setRight(five);
+	    three.setLeft(six);
+	    three.setRight(seven);
+	    four.setLeft(eight);
+	    four.setRight(nine);
+	    five.setLeft(ten);
+	    five.setRight(eleven);
+	    six.setLeft(twelve);
+	    six.setRight(thirteen);
+	    seven.setLeft(fourteen);
+	    
+	    BinaryTree<Integer> bt = new BinaryTree<Integer>(one);
+	    
+	    System.out.println("Height: " +  bt.height(one));
+	    System.out.println("Level Order Recursion: ");
+	    bt.printLevelOrderRecursion();
+	    System.out.println("Level Order Using Queue Iterative: ");
+	    bt.printLevelOrderIterativeLineByLine(one);
+	    System.out.println();
+	    System.out.println("preorder traversal: ");
+	    bt.preorderTraversalPrint(one);
+	    System.out.println();
+	    System.out.println("inorder traversal: ");
+	    bt.inorderTraversalPrint(one);
+	    System.out.println();
+	    System.out.println("inorder traversal Iterative: ");
+	    bt.inorderTraversalPrintIterative(one);
+	    System.out.println();
+	    System.out.println("postorder traversal: ");
+	    bt.postorderTraversalPrint(one);
 	}
 }
